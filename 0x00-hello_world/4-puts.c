dfe0219b11163f6faf643b22e6fc90df8e9ba018
Stellamaris-ux <marisangel09@yahoo.com>
2023-09-08 00:01:22 +0100

    Learning C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,4 @@
-#include <stdio.h>
-
+#include<stdio.h>
 /**
  *  * main - Entry point
  *   *
@@ -7,5 +6,7 @@
  *     */
 int main(void)
 {
-	    return (0);
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
 }
+
