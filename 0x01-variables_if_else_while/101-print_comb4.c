4c698047913a6160d53dd14b5f433ec590e1a33d
Stellamaris-ux <marisangel09@yahoo.com>
2023-09-08 23:43:08 +0100

    Variables in C

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -2,13 +2,13 @@
 
 /**
  * main - prints all possible different combinations of three digits
- * 
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int a; 
-	int b; 
+	int a;
+	int b;
 	int c;
 
 	for (a = 48; a < 58; a++)
