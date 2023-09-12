620f0482152dda1bfc1baaeb265d8cd5c5867423
Stellamaris-ux <marisangel09@yahoo.com>
2023-09-12 14:19:46 +0100

    Fuctions, Nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - in lowercase
+ *
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char k;
+
+	for (k = 'a'; k <= 'z'; k++)
+		_putchar(k);
+
+	_putchar('\n');
+}
