856d424c4dd9498d5369af66d9a25b3385b2933a
Stellamaris-ux <marisangel09@yahoo.com>
2023-09-12 15:13:20 +0100

    Fuctions, Nested loops

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ * starting from 00:00 to 23:59
+ */
+void jack_bauer(void)
+{
+	int n, p;
+
+	n = 0;
+
+	while (n < 24)
+	{
+		p = 0;
+		while (p < 60)
+		{
+			_putchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
+			_putchar(':');
+			_putchar((p / 10) + '0');
+			_putchar((p % 10) + '0');
+			_putchar('\n');
+			p++;
+		}
+		i++;
+	}
+}
+
