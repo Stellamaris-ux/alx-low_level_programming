6e7383f1b8c2d2e1807f6a85aaf4497268cf0b1c
Stellamaris-ux <marisangel09@yahoo.com>
2023-09-12 14:40:51 +0100

    Fuctions, Nested loops

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character
+ *
+ * Return: 1 if var is a letter, else return 0
+ */
+int _isalpha(int c)
+{
+	return ((c >= 97 && c <= 122) || (c >= 65 && c <= 90));
+}
+
